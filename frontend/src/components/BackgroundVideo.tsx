70910bbdb335e56c5b590285791f39176a1ddedd
Or Rodrigez <orrodrigez1@gmail.com>
2023-09-21 14:19:47 +0300

    Move video to public

--- frontend/src/components/BackgroundVideo.tsx
+++ frontend/src/components/BackgroundVideo.tsx
@@ -1,10 +1,13 @@
-import videoBG from "@/assets/videoBG.mp4";
 import "@/styles/backgroundVideo.css";
 
 export default function BackgroundVideo() {
   return (
     <video autoPlay muted loop>
-      <source className="background-video" src={videoBG} type="video/mp4" />
+      <source
+        className="background-video"
+        src="/videoBG.mp4"
+        type="video/mp4"
+      />
     </video>
   );
 }
