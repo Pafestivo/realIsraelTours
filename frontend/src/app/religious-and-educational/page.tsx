b250bb0103e572402d0cfa300bdbd84e962258ad
Or Rodrigez <orrodrigez1@gmail.com>
2023-09-22 19:14:04 +0300

    Finish all the tours pages

--- frontend/src/app/religious-and-educational/page.tsx
+++ frontend/src/app/religious-and-educational/page.tsx
@@ -1,17 +1,29 @@
 import SingleTourPage from "@/components/SingleTourPage";
+import TabSwitcher from "@/components/TabSwitcher";
 import React from "react";
+import { educationalTripsArray } from "../utils/EducationalToursArray";
 
 const ReligiousAndEducational = () => {
   return (
-    <SingleTourPage
-      tourTitle="Community, Religious & Educational Tours"
-      tourBanner="/ReligiousToursChristianOrgs.jpg"
-      promoTitle="Venture on a Spiritual Journey"
-      promoDescription={[
-        `Our staff are experts in spiritual empowerment in the Holy Land and tailor-making deeply moving experiences for all types of communities.
-      Jewish or Christian, whatever your group’s denomination, level of religiosity, age group, or size, we build religious trips that have touched the souls of all kinds of congregations, organizations, or youth groups.`,
-      ]}
-    />
+    <div>
+      <SingleTourPage
+        tourTitle="Community, Religious & Educational Tours"
+        tourBanner="/ReligiousToursChristianOrgs.jpg"
+        promoTitle="Venture on a Spiritual Journey"
+        promoDescription={[
+          `Our staff are experts in spiritual empowerment in the Holy Land and tailor-making deeply moving experiences for all types of communities.
+        Jewish or Christian, whatever your group’s denomination, level of religiosity, age group, or size, we build religious trips that have touched the souls of all kinds of congregations, organizations, or youth groups.`,
+        ]}
+      />
+
+      <section
+        style={{
+          background: "white",
+        }}
+      >
+        <TabSwitcher array={educationalTripsArray} />
+      </section>
+    </div>
   );
 };
 
---
+++ frontend/src/app/utils/EducationalToursArray.tsx
@@ -0,0 +1,21 @@
+export const educationalTripsArray = [
+  {
+    name: "Federation Trips",
+    description: `Real Israel Tours leads Jewish Federation trips, addressing the unique missions, needs and interests of each delegation. Our team has worked with many different Jewish organizations from across the world from California to Australia and South Africa. We are passionate about an Israel trip’s ability to invigorate Jewish communities and inspire Jewish leaders. Through our vast variety of creative resources and professional expertise, participants experience an emotional as well as educational journey, strengthening their Jewish identities.`,
+    image: "/Negevpic.jpg",
+  },
+  {
+    name: "Jewish Organizations",
+    description: `We also cater to synagogues, youth groups, JCCs, Jewish schools, Jewish camps, and any other religious institution that might be interested in organizing a life-changing journey for their community. Our team spends time getting to know your community and discussing what experiences would be most compelling for your particular demographic, from classic to off-the-beaten path activities and ideas for unique spiritual moments they will never forget.`,
+    image: "/Negevpic.jpg",
+  },
+  {
+    name: "Christian Organizations",
+    description: `Real Israel Tours also offers powerful spiritual Christian trips to the Holy Land. Walk where Jesus walked guided by experts in biblical history and watch the Bible come to life. Participants feel struck with spiritual energy on our journeys through the most important sites in Christianity. Near the Galilee, choose to visit Nazareth, Jesus’s hometown, Capernaum, where Jesus began his ministry, preached, and performed miracles, and the Mount of Beatitudes, where he preached the Sermon on the Mount. We offer opportunities to submerge yourself in the Jordan River, where Jesus was baptized by John the Baptist, and sail on the Sea of Galilee as the disciples did. In Jerusalem, whether you want a more educational tour or a spiritual procession, you will walk Via Dolorosa, the way of sorrows/suffering, the path Jesus carried his cross through Jerusalem’s old city to the Church of Holy Sepulchre, where he was crucified and buried. We also recommend a pilgrimage to the oldest continuous site of worship in Christianity, the Church of Nativity in Bethlehem.
+
+    You can also take the opportunity to visit any of Israel’s dozens of other powerful Christian sites. Choose to visit the Mount of Olives, the Church of All Nations, Mount Tabor, Valley of Elah, Tabgha, The Garden Tomb, Mount Zion, the room of the Last Supper, Caesarea National Park, Mount Sodom, or others. In addition to compelling tours of the most famous sites, we are well versed in any and all Christian sites across the Holy Land, ready to suggest additional unique places and activities that might peak your unique group’s interests.
+    
+    We accommodate anything from small family trips to larger Christian organizations and look forward to customizing a trip with your group’s preferred balance of history and spirituality, creative experiences and archeological sites, fun activities and empowering moments.`,
+    image: "/Negevpic.jpg",
+  },
+];
